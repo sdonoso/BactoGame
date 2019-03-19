dd742740efbda73c48ead1bc4c559cd79fac1f32
Sebastian Donoso <sebastian.a.donoso.b@gmail.com>
2019-03-18 21:29:00 -0300

    clean error

--- tarea0/todos/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from  todos.models import Todo
-
-class ToDoForm(forms.Form):
-    tarea = forms.CharField(required=True, widget = forms.TextInput(attrs={'placeholder' : 'Nueva Tarea'}))
